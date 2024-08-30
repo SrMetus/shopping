205ea97f820fbc3c1c61409fcd60cee72d111d15
SrMetus <s.gaggero87@gmail.com>
2024-08-29 20:06:31 -0400

    Creacion de paginas y enrutamiento

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function Home() {
+    return (
+      <>
+        <div className="bg-red-400">
+          Home
+        </div>        
+      </>
+    )
+  }
+  
+  export default Home
